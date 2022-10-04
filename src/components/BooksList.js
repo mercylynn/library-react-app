1e4f4323d7b1ee8970b41067c368d23a921949d3
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-04 22:40:14 +0300

    add filter function and passed props

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,13 +1,20 @@
+import { functions } from 'lodash'
 import React from 'react'
 import Bookcard from './BookCard'
 
-function BooksList({ books }) {
+function BooksList({ books, setBooks }) {
+    function deleteBook(deletedBook) {
+        setBooks(books.filter((book) => {
+            return book.id !== deletedBook.id
+        }))
+
+    }
     return (
         <div>
             {books.map((book) => {
                 return (
                     <div>
-                        <Bookcard key={book.id} book={book} />
+                        <Bookcard key={book.id} book={book} deleteBook={deleteBook} />
                     </div>
                 );
             })
