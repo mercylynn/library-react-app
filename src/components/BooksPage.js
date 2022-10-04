5b4293364c72533a720bafdba47623d2a61500d4
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-04 19:02:29 +0300

    pass prop

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search';
 import BooksList from './BooksList'
 
 function BooksPage() {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/books')
+            .then(resp => resp.json())
+            .then(data => {
+                setBooks(data)
+            })
+    }, [])
     return (
         <div>BooksPage
 
 
-            <BooksList />
+            <BooksList books={books} />
 
         </div>
     );
