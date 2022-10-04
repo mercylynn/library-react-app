3882b8898922e8d7532bc047812f55b93dfdd7f0
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-04 14:40:17 +0300

    import component

---
+++ src/components/BooksList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bookcard from './BookCard'
+
+function BooksList() {
+    return (
+        <div>BooksList
+            <Bookcard />
+        </div>
+    )
+}
+
+export default BooksList
