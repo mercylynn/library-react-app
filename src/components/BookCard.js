b3fd1a674bd88c32d4eaa9c9a151689f6800788e
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-04 14:39:55 +0300

    add card jsx

---
+++ src/components/BookCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function BookCard() {
+  return (
+    <div className='container'>
+      <div className='card'>
+
+        <div>image</div>
+        <div className=''>
+          <h2>name</h2>
+          <p>author</p>
+          <p>year</p>
+        </div>
+        <div className="">
+          <button
+            className=""
+          >
+            x
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default BookCard
