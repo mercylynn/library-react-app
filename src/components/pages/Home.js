00ad31ba12d5dbcf4290b141f79f1bce60684376
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:51:23 +0300

    added home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Pages.css'
+
+function Home() {
+    return (
+        <div className='homecontainer'>
+            <h1>Find A Programming Book today!</h1>
+        </div>
+    )
+}
+
+export default Home
