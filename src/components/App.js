1bc9d6afb73f361d9320098424f99328670dcb0b
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:46:51 +0300

    import browserrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom"
 import BooksPage from "./BooksPage"
+import Navbar from "./Navbar";
+import About from "./pages/About"
+import AddBookForm from "./AddBookForm";
+import Footer from "./Footer/Footer"
+import Login from "./Login";
+import Home from "./pages/Home"
+
 
 function App() {
+    useEffect(() => {
+        document.title = 'library-books';
+    });
     return (
         <div>
-            <BooksPage />
+
+
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/books' element={<BooksPage />} />
+                <Route path='/add' element={<AddBookForm />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+            <Footer />
+
         </div>
     )
+
 }
 export default App;
