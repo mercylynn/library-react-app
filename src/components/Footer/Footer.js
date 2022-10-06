50bb2d7ef5945c4c2718e4937298905c09962947
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:50:30 +0300

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <footer className="copyright">
+                <p>Copyright &copy; 2022 Books App | All Rights Reserved</p>
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
