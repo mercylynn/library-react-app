7a2baaa2ea818065402d521c80546d097340355b
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 21:45:13 +0300

    add content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Pages.css'
 
 function us() {
     return (
-        <div>About us</div>
+        <div className='about'>
+            <h4>A programming <span className='tittle'>book library</span> where users can search for a book in the booklist,</h4>
+            <strong><p>filter according to different programming languages,</p></strong>
+            <strong><p>add a book</p></strong>
+            <strong><p>and also delete it from the bookCollection</p></strong>
+
+
+
+
+        </div>
     )
 }
 
