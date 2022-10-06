a582a845a9febb7c8bc8302474cf68e3043b7767
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:48:24 +0300

    added search and filter functions

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,25 +1,72 @@
-import { functions } from 'lodash'
-import React from 'react'
+
+import React, { useState } from 'react'
 import Bookcard from './BookCard'
+import Search from './Search'
+
 
 function BooksList({ books, setBooks }) {
+    const [category, setCategory] = useState("All")
+    const [search, setSearch] = useState("")
+
     function deleteBook(deletedBook) {
         setBooks(books.filter((book) => {
             return book.id !== deletedBook.id
         }))
 
     }
+
+
+
+
+    function handleCategoryChange(event) {
+        setCategory(event.target.value);
+    }
+    function handleSearchChange(e) {
+        setSearch(e.target.value)
+    }
+    const booksToDisplay = books
+        // category
+        .filter(
+            (book) => category === "All" || book.category === `${category}`
+        )
+        // search term
+        .filter((book) => book.name.toLowerCase().includes(search.toLowerCase()));
+
+    const displayBooks = booksToDisplay.map((eventObj) => {
+        return <Bookcard key={eventObj.id} book={eventObj} deleteBook={deleteBook} />
+    })
+    //Render events in rows of 4
+    //Mutates displayEvents into arrays of groups of 4
+    function renderBooks() {
+        let books = []
+        let size = 4
+
+        for (let i = 0; i < displayBooks.length; i += size) {
+            books.push(displayBooks.slice(i, i + size))
+        }
+        const renderBooks = books.map((booksRow) => {
+            return <div className='books'>{booksRow}</div>
+        })
+
+        return renderBooks
+    }
     return (
         <div>
-            {books.map((book) => {
+            {/* {books.map((book) => {
                 return (
                     <div>
                         <Bookcard key={book.id} book={book} deleteBook={deleteBook} />
+                        
+
                     </div>
                 );
             })
 
-            }
+            } */}
+            <Search search={search} onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
+            <div>{renderBooks()}</div>
+
+
 
         </div>
     )
