e2c34c4190a309130107388806f213768c85ce4f
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:49:26 +0300

    added login feature

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+
+function Login() {
+
+    let navigate = useNavigate()
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(event) {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    //Redirect to home page
+    function handleSubmit(e) {
+        e.preventDefault()
+        setFormData({
+            email: "",
+            password: ""
+        })
+        return navigate('/')
+    }
+
+    return (
+        <div>
+            <h4>Login</h4>
+            <form className='loginForm' onSubmit={handleSubmit}>
+                <input className='inputform' placeholder='Email' type='text' name='email' value={formData.email} onChange={handleChange} />
+                <input className='inputform' placeholder='Password' type='password' name='password' value={formData.password} onChange={handleChange} />
+                <button type='submit' value='Log in' className='button'>login </button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
