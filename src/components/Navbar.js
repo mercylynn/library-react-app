6f37716ee335c1eb4395ba52e2bef156e4e8f366
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-06 10:49:47 +0300

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+        <div>
+            <nav className='navbar'>
+                <div className="nav-container">
+                    <NavLink exact to="/" className="nav-logo">
+                        LibraryBooks
+                        <i className="fas fa-code"></i>
+                    </NavLink>
+
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/"
+                                activeclassname="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                Home
+
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-item">
+                            <NavLink
+                                to="/about"
+                                activeclassname="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/books"
+                                activeclassname="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                Books
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/add"
+                                activeclassname="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                AddBook
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/login"
+                                activeclassname="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                Login
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <div className="nav-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
